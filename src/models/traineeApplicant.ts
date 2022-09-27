ebf42b88c07d4bc228b15dea44b952758bd5f007
BenIraa <iradukundacyuzuzo50@gmail.com>
2022-09-27 19:05:16 +0200

    SOFT DELETE

--- src/models/traineeApplicant.ts
+++ src/models/traineeApplicant.ts
@@ -1,18 +1,26 @@
-import { model,Schema } from "mongoose";
-const traineeSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required:true,
-    }, 
-})
-export const TraineeApplicant = model("Trainee", traineeSchema);
-// trainees
+import mongoose, { model,Schema } from "mongoose";
+const TraineeApplicant = mongoose.model(
+    "traineeSchema",
+    new Schema({
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        firstname: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required:true,
+        },
+        delete_at: {
+            type: Boolean, 
+            default: false,
+     
+        }
+    })
+);
+
+export default TraineeApplicant;
