52c8f9a738de0469ec5666a895c684b722a26c01
kareraolivier <kareraolivier@gmail.com>
2022-09-27 12:41:47 +0200

    Application cycle CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
-import { ApolloServer, gql } from "apollo-server";
-import { connect } from "./database/db.config";
-import loadTraineeResolver from "./resolvers/traineeResolvers";
-import { mergeResolvers } from "@graphql-tools/merge";
-import typeDefs from "./schema/index";
+import { ApolloServer, gql } from "apollo-server"
+import { mergeResolvers, mergeTypeDefs } from '@graphql-tools/merge'
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import { connect } from "./database/db.config"
+import applicationCycleResolver from "./resolvers/applicationCycleResolver"
+import applicationCycleTypeDefs from './schema/applicationCycleTypeDefs'
 
 const PORT = process.env.PORT || 4001;
 
-// const typeDefs = gql`
-//   type Query {
-//     hello: String
-//   }
-// `;
-
-const resolvers = mergeResolvers([loadTraineeResolver]);
-
+const resolvers = mergeResolvers([applicationCycleResolver])
+const typeDefs= mergeTypeDefs([applicationCycleTypeDefs])
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-});
+  introspection: true,
+  csrfPrevention: true,
+  plugins: [ApolloServerPluginLandingPageLocalDefault],
+  cache: 'bounded',
+})
 
 connect().then(() => {
   console.log("Database connected!");
