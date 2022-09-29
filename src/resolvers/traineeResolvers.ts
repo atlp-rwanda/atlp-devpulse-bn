bb6bf6ed4a18772b905f01caee70f82c7a713433
mugiisha <mugishabankz3@gmail.com>
2022-09-29 14:09:15 +0200

    catching errors

--- src/resolvers/traineeResolvers.ts
+++ src/resolvers/traineeResolvers.ts
@@ -10,55 +10,59 @@ const loadTraineeResolver: any = {
   },
   Mutation: {
     async loadTrainees(_parent: any, _args: any) {
-      const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-      });
+      try {
+        const auth = new google.auth.GoogleAuth({
+          keyFile: "credentials.json",
+          scopes: "https://www.googleapis.com/auth/spreadsheets",
+        });
 
-      //create client instance for auth
-      const client = await auth.getClient();
+        //create client instance for auth
+        const client = await auth.getClient();
 
-      //instance of google sheets API
-      const googleSheets = google.sheets({
-        version: "v4",
-        auth: client,
-      });
+        //instance of google sheets API
+        const googleSheets = google.sheets({
+          version: "v4",
+          auth: client,
+        });
 
-      //read rows from spreadsheet
-      const spreadsheetId = _args.spreadsheetId;
-      const rows = await googleSheets.spreadsheets.values.get({
-        auth,
-        spreadsheetId,
-        range: "Sheet1",
-      });
-      //loop through rows and add them to our db
-      if (rows.data.values !== undefined && rows.data.values !== null) {
-        for (let i = 1; i < rows.data?.values?.length; i++) {
-          const trainee = new Trainee({
-            firstName: rows.data.values[i][1],
-            lastName: rows.data.values[i][2],
-            gender: rows.data.values[i][3],
-            email: rows.data.values[i][4],
-            age: rows.data.values[i][5],
-            phoneNumber: rows.data.values[i][6],
-            fieldOfStudy: rows.data.values[i][7],
-            highOrCurrentEducation: rows.data.values[i][8],
-            province: rows.data.values[i][9],
-            district: rows.data.values[i][10],
-            cohort: rows.data.values[i][11],
-            employmentStatus: rows.data.values[i][12],
-            isStudent: rows.data.values[i][13],
-            hackerrankScore: rows.data.values[i][14],
-            englishScore: rows.data.values[i][15],
-            interview: rows.data.values[i][16],
-            decision: rows.data.values[i][17],
-            pastAndela: rows.data.values[i][18],
-          });
-          await trainee.save();
+        //read rows from spreadsheet
+        const spreadsheetId = _args.spreadsheetId;
+        const rows = await googleSheets.spreadsheets.values.get({
+          auth,
+          spreadsheetId,
+          range: "Sheet1",
+        });
+        //loop through rows and add them to our db
+        if (rows.data.values !== undefined && rows.data.values !== null) {
+          for (let i = 1; i < rows.data?.values?.length; i++) {
+            const trainee = new Trainee({
+              firstName: rows.data.values[i][1],
+              lastName: rows.data.values[i][2],
+              gender: rows.data.values[i][3],
+              email: rows.data.values[i][4],
+              age: rows.data.values[i][5],
+              phoneNumber: rows.data.values[i][6],
+              fieldOfStudy: rows.data.values[i][7],
+              highOrCurrentEducation: rows.data.values[i][8],
+              province: rows.data.values[i][9],
+              district: rows.data.values[i][10],
+              cohort: rows.data.values[i][11],
+              employmentStatus: rows.data.values[i][12],
+              isStudent: rows.data.values[i][13],
+              hackerrankScore: rows.data.values[i][14],
+              englishScore: rows.data.values[i][15],
+              interview: rows.data.values[i][16],
+              decision: rows.data.values[i][17],
+              pastAndela: rows.data.values[i][18],
+            });
+            await trainee.save();
+          }
         }
-      }
 
-      return "Trainees loaded to db successfully";
+        return "Trainees loaded to db successfully";
+      } catch (error) {
+        return error;
+      }
     },
   },
 };
