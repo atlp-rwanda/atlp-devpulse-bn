e3dc90e532c03d6eed9848e380edb9f4f94adfbc
Gedeonir <irafasha.jedy12@gmail.com>
2022-10-24 14:44:54 +0200

    List of all deleted trainee
    List of all deleted trainee

--- src/resolvers/emptyrecycle.ts
+++ src/resolvers/emptyrecycle.ts
@@ -1,6 +1,38 @@
 import TraineeApplicant from "../models/traineeApplicant";
 
 export  const recyclebinresolver:any ={
+    Query: {
+        async allSoftDeletedTrainees(_: any, { input }: any) {
+          // define page
+          const { page, itemsPerPage, All } = input;
+          let pages;
+          let items;
+    
+          if (page) {
+            pages = page;
+          } else {
+            pages = 1;
+          }
+          if (All) {
+            // count total items inside the collections
+            const totalItems = await TraineeApplicant.countDocuments({});
+            items = totalItems;
+          } else {
+            if (itemsPerPage) {
+              items = itemsPerPage;
+            } else {
+              items = 3;
+            }
+          }
+          // define items per page
+          const itemsToSkip = (pages - 1) * items;
+          const allSoftDeletedTrainee = await TraineeApplicant.find({delete_at:true})
+            .skip(itemsToSkip)
+            .limit(items);
+          return allSoftDeletedTrainee;
+        },
+    },
+
     Mutation:{
         async emptyRecyclebin(_:any){
             try {
