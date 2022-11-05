4c355a9a0738961cec0b6c81f27036d0d711b5d0
UmuhireJessie <86103578+UmuhireJessie@users.noreply.github.com>
2022-11-05 13:47:33 +0200

    changes to make hiding work

--- src/resolvers/filterTraineeResolver.ts
+++ src/resolvers/filterTraineeResolver.ts
@@ -29,12 +29,22 @@ const filterTraineeResolver: any = {
 
       const allTraineeAttribute = await traineEAttributes
         .find({})
-        .populate("trainee_id")
+        .populate({
+          path: "trainee_id",
+          populate: {
+            path: "cycle_id",
+            model: "applicationCycle",
+          },
+        })
         .skip(itemsToSkip)
         .limit(items);
 
+      const nonNullTrainee = allTraineeAttribute.filter((value) => {
+        return value !== null;
+      });
+
       if (wordEntered && !filterAttribute) {
-        const filterResult = allTraineeAttribute.filter((value: any) => {
+        const filterResult = nonNullTrainee.filter((value: any) => {
           return (
             value._id
               .toString()
