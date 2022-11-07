06fb965d79b2165dcdbca0cac2394b8788e5907a
Amiir <mugishabankz3@gmail.com>
2022-11-07 18:20:27 +0200

    fix trainee applicant db calls missing cycle relation (#36)

--- src/schema/deleteTraineTypeDefs.ts
+++ src/schema/deleteTraineTypeDefs.ts
@@ -1,38 +1,35 @@
 import { gql } from "apollo-server";
 const Schema = gql`
+  type Trainee {
+    id: ID!
+    email: String!
+    firstName: String!
+    lastName: String!
+    delete_at: Boolean
+    cycle_id: applicationCycle
+  }
 
-type Trainee{
-    id:ID!
-    email:String!
-    firstName:String!
-    lastName:String!
-    delete_at:Boolean
-}
+  input softdeleteTrainee {
+    id: ID!
+  }
+  input softRecover {
+    id: ID!
+    delete_at: Boolean
+  }
+  input deleteTrainee {
+    id: ID!
+  }
 
-input softdeleteTrainee{
-    id:ID!
-   
-    
-}
-input softRecover{
-    id:ID!
-    delete_at:Boolean
-    
-}
-input deleteTrainee{
-    id:ID!
-}
+  type Query {
+    getAllTrainees: [Trainee]
+    traineeSchema(id: ID!): Trainee!
+    getAllSoftDeletedTrainees: [Trainee]
+  }
 
-type Query{
-    getAllTrainees:[Trainee]
-    traineeSchema(id: ID!):Trainee!
-    getAllSoftDeletedTrainees : [Trainee]
-}
-
-type Mutation {
-    deleteTrainee(id:ID!):Trainee!
-    softdeleteTrainee(input: softdeleteTrainee ):Trainee!
-    softRecover(input: softRecover ):Trainee!
-}
-`
-export default Schema
+  type Mutation {
+    deleteTrainee(id: ID!): Trainee!
+    softdeleteTrainee(input: softdeleteTrainee): Trainee!
+    softRecover(input: softRecover): Trainee!
+  }
+`;
+export default Schema;
