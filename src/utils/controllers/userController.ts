31aa70e676887ebff8d2044e1adb394e40bf3f75
UN Francois Rudasingwa <86821764+un-anelka@users.noreply.github.com>
2023-01-11 16:34:05 +0200

    Add user if does not exist after merging import trainee (#53)

---
+++ src/utils/controllers/userController.ts
@@ -0,0 +1,34 @@
+import { OAuth2Client } from "google-auth-library";
+import { LoggedUserModel } from "../../models/AuthUser";
+const client = new OAuth2Client(process.env.OAUTH_CLIENT_ID);
+
+export const findOrCreateUser = async (token: any) => {
+  //verify token
+  const googleUser = await verifyAuthToken(token);
+  //check existance of user
+  const user = await checkIfUserExists(googleUser?.email);
+
+  return user ? user : createNewUser(googleUser);
+};
+
+const verifyAuthToken = async (token: any) => {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.OAUTH_CLIENT_ID,
+    });
+
+    return ticket.getPayload();
+  } catch (error) {
+    console.error("Error verifying token", error);
+  }
+};
+
+const checkIfUserExists = async (email: any) =>
+  await LoggedUserModel.findOne({ email }).exec();
+
+const createNewUser = async (googleUser: any) => {
+  const { name, email, picture } = googleUser;
+  const user = { name, email, picture, createdAt: new Date().toISOString() };
+  return new LoggedUserModel(user).save();
+};
