22b829862e341ea6344bc19bb7e37e7fb0e04ac5
Edcreation <93517096+Edcreation@users.noreply.github.com>
2023-08-01 12:53:00 +0200

    ch(git-action): set up git actions and tests

---
+++ src/test/index.test.ts
@@ -0,0 +1,8 @@
+import { assert, expect } from "chai";
+
+describe("This is a demo test", () => {
+      it("should return 5 when 2 is added to 3", () => {
+      const result = 2 +  3;
+      expect(result).eql(5);
+   });
+});
