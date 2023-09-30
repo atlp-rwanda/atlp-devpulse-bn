3f67e29e3ad720930d7b6883b11fcc382e56ad33
JENNY-257 <j.iribagiza2@gmail.com>
2023-09-30 09:30:34 +0200

    ft(update and view):update and view own application

--- src/models/permissionModel.ts
+++ src/models/permissionModel.ts
@@ -2,68 +2,49 @@ import { model, Schema } from 'mongoose';
 
 const permissionSchema = new Schema({
   entity: {
-    type: String,
-    required: true,
+    type:String,
+    enum:["Users","Application","Cohorts","Role","Finance"]
   },
   create: {
-    isPermitted: {
       type: Boolean,
       default: false,
-    },
   },
   viewOwn: {
-    isPermitted: {
       type: Boolean,
       default: false,
-    },
   },
   viewMultiple: {
-    isPermitted: {
       type: Boolean,
       default: false,
-    },
   },
   viewOne: {
-    isPermitted: {
       type: Boolean,
       default: false,
     },
-  },
   updateOwn: {
-    isPermitted: {
       type: Boolean,
       default: false,
-    },
   },
   updateMultiple: {
-    isPermitted: {
       type: Boolean,
       default: false,
-    },
+
   },
   updateOne: {
-    isPermitted: {
       type: Boolean,
       default: false,
-    },
   },
   deleteOwn: {
-    isPermitted: {
       type: Boolean,
       default: false,
-    },
   },
   deleteMultiple: {
-    isPermitted: {
       type: Boolean,
       default: false,
-    },
   },
   deleteOne: {
-    isPermitted: {
       type: Boolean,
       default: false,
-    },
   },
 });
 
