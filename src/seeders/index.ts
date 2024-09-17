79be07b2746bb95488e187054040c71e9d9b1b8d
uwituzeb <b.uwituze@alustudent.com>
2024-09-17 23:30:41 +0200

    admin user seed

--- src/seeders/index.ts
+++ src/seeders/index.ts
@@ -5,8 +5,10 @@ import seedDeleteTrainee from './DelTrainee';
 import seedJobs from './jobs';
 import seedPrograms from './programs';
 import seedCohorts from './cohorts';
+import seedUsers from './users';
 
 connect().then(async () => {
+    await seedUsers();
     await Applicationseed()
     await      seedDeleteTrainee()
     await seedPrograms();
---
+++ src/seeders/users.ts
@@ -0,0 +1,33 @@
+import { LoggedUserModel } from "../models/AuthUser";
+import { RoleModel } from "../models/roleModel";
+
+const seedUsers = async() => {
+        const superAdminRole = await RoleModel.findOne({ roleName: 'superAdmin' });
+        const applicantRole = await RoleModel.findOne({ roleName: 'applicant' });
+
+        if(!superAdminRole || !applicantRole){
+            console.error("Roles not found");
+            return;
+        }
+
+        const users = [
+            {
+                firstname: "Super",
+                lastname: "Admin",
+                email: "admin@example.com",
+                password: "password123",
+                role: superAdminRole._id,
+                country: "Rwanda",
+                code: "+250",
+                telephone: "0788888888",
+                isActive: true,
+                gender: "female",
+
+            }
+        ]
+
+        await LoggedUserModel.insertMany(users);
+
+}
+
+export default seedUsers;
