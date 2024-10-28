f77cddff781956b196a3700d522f72673de11ae9
chris <iradukundachristian202@gmail.com>
2024-10-28 22:38:59 +0200

    ticket create-notifications

--- src/resolvers/ticketResolver.ts
+++ src/resolvers/ticketResolver.ts
@@ -1,6 +1,10 @@
 import { ticketModel } from "../models/ticketModel";
 import { LoggedUserModel } from "../models/AuthUser";
 import { CustomGraphQLError } from "../utils/customErrorHandler";
+import { ApplicantNotificationsModel } from "../models/applicantNotifications";
+import { RoleModel } from "../models/roleModel";
+import { pusher } from "../helpers/pusher";
+import { sendEmailTemplate } from "../helpers/bulkyMails";
 
 export const ticketResolver = {
     Query: {
@@ -74,6 +78,37 @@ export const ticketResolver = {
                     author: user._id,
                     status: "Open"
                 });
+                
+                const message = `Your ticket "${args.title}" has been submitted successfully.`;
+                const notification = await ApplicantNotificationsModel.create({
+                    userId: user._id,
+                    message,
+                    eventType: "general",
+                });
+
+                await pusher
+                    .trigger(`notifications-${user._id}`, "new-notification", {
+                        message: notification.message,
+                        id: notification._id,
+                        createdAt: notification.createdAt,
+                        read: notification.read,
+                    })
+                    .catch((error) => {
+                        console.error("Error with Pusher trigger:", error);
+                    });
+                await sendEmailTemplate(
+                    user.email,
+                    "Ticket Received",
+                    `Hello ${user.email.split("@")[0]}, `,
+                    `Your Ticket titled:
+                     <strong>${args.title}</strong>
+                    has been received and will be reviewed soon.
+                    <br />
+                    <br />
+                    Thank you for your patience.
+              `
+            );
+
                 return newTicket.populate('author');
             } catch(error: any) {
                 throw new CustomGraphQLError(error.message);
