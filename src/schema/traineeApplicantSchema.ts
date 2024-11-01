6166ebef9ecb9169f335f5846c293e852f7fbfc4
@Robs_dagreat <robsrwibutso12@gmail.com>
2024-11-01 14:59:15 +0200

    fix(#156): Have a user be able to apply as a trainee (#132)
    * fix(#156): Have a user be able to apply as a trainee
    
    * fix(#156):Have notifications integrated when a trainee apply

--- src/schema/traineeApplicantSchema.ts
+++ src/schema/traineeApplicantSchema.ts
@@ -1,16 +1,19 @@
 import { gql } from "apollo-server";
+
 export const typeDefsTrainee = gql`
   type Query {
     allTrainees(input: pagination): response
     getOneTrainee(ID: ID!): traineeApplicant
     getTraineeByUserId(userId: ID!): ID!
   }
+
   type response {
     totalItems: Int!
     page: Int!
     itemsPerPage: Int!
     data: [traineeApplicant]!
   }
+
   type Mutation {
     createNewTraineeApplicant(
       input: newTraineeApplicantInput
@@ -22,17 +25,36 @@ export const typeDefsTrainee = gql`
     deleteTraineeApplicant(email: String!): Boolean
     acceptTrainee(traineeId: ID!, cohortId: ID!): AcceptTraineeResponse!
   }
+
   type traineeApplicant {
     lastName: String!
     firstName: String!
     _id: ID!
     email: String!
     cycle_id: applicationCycle
+    cycleApplied: [CycleApplied!]!
     delete_at: Boolean
     status: String!
     applicationPhase: String!
     cohort: ID
+    user: User
   }
+
+  type CycleApplied {
+    _id: ID!
+    cycle: ApplicationCycle!
+  }
+
+  type User {
+    _id: ID!
+  }
+
+  type applicationCycle {
+  _id: ID!
+  name: String!
+  startDate: String!
+  endDate: String!
+}
 
   type AcceptTraineeResponse {
     success: Boolean!
@@ -50,12 +72,12 @@ export const typeDefsTrainee = gql`
   }
 
   input newTraineeApplicantInput {
-  lastName: String!
-  firstName: String!
-  email: String!
-  cycle_id: String!
-  attributes: TraineeAttributeInput
-}
+    lastName: String!
+    firstName: String!
+    email: String!
+    cycle_id: ID!
+    attributes: traineeAttributeInput
+  }
 
   input traineeApplicantEmail {
     email: String!
@@ -64,7 +86,7 @@ export const typeDefsTrainee = gql`
   input traineeApplicantInputUpdate {
     firstName: String
     lastName: String
-    cycle_id: String!
+    cycle_id: ID
     status: String
   }
 
