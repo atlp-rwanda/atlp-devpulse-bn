d485261f49c08c8776e2a813b5210e5b824b0419
jkarenzi <j.karenzi@alustudent.com>
2024-11-10 14:53:00 +0200

    feat(job-application): Applicant should be able to apply to job post
    - create mutation and schema to handle job applications
    
    [Delivers #140]

---
+++ src/schema/jobApplicationSchema.ts
@@ -0,0 +1,63 @@
+import { gql } from "apollo-server";
+
+export const jobApplicationTypeDefs = gql`
+    type Job {
+        _id: ID!
+        title: String!
+    }
+
+    type User {
+        _id: ID!
+        email: String!
+        gender: String!
+        firstname: String!
+        lastname: String!
+        country: String!
+        telephone: String!
+    }
+
+    type JobApplication {
+        _id: ID!
+        userId: User!
+        jobId: Job!
+        essay: String!
+        resume: String!
+        status: String!
+        createdAt: String!
+    }
+
+    input JobApplicationInput {
+        jobId: ID!
+        essay: String!
+        resume: String!
+    }
+
+    input SingleJobApplicationInput {
+        applicationId: ID!
+    }
+
+    input StatusInput {
+        applicationId: ID!
+        status: String!
+    }
+
+    type checkIfUserAppliedOutput {
+        status: Boolean!
+    }
+
+    input CheckIfUserAppliedInput {
+        jobId: ID!
+    }
+
+    type Query {
+        getOwnJobApplications: [JobApplication!]!
+        getAllJobApplications: [JobApplication!]!
+        getOneJobApplication(input: SingleJobApplicationInput): JobApplication!
+        checkIfUserApplied(input: CheckIfUserAppliedInput): checkIfUserAppliedOutput!
+    }
+
+    type Mutation {
+        createNewJobApplication(input: JobApplicationInput!): JobApplication!
+        changeApplicationStatus(input: StatusInput!): JobApplication!
+    }
+`
