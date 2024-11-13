cf75e82a48f6f9ddbd297539e31cc734903e20e1
El-zizo Mugisha <El-zizo@Mugishas-MacBook-Pro.local>
2024-11-13 13:37:22 +0200

    update applicant status to Admitted/Accepted and change role to trainee

--- src/models/traineeApplicant.ts
+++ src/models/traineeApplicant.ts
@@ -3,15 +3,16 @@ import mongoose, { Schema } from "mongoose";
 const CycleAppliedSchema = new Schema({
   cycle: {
     type: Schema.Types.ObjectId,
-    ref: 'ApplicationCycle',
-    required: true
+    ref: "ApplicationCycle",
+    required: true,
   },
-})
+});
 
-const TraineeApplicantSchema = new Schema({
+const TraineeApplicantSchema = new Schema(
+  {
     user: {
       type: Schema.Types.ObjectId,
-      ref: 'User'
+      ref: "User",
     },
     email: {
       type: String,
@@ -26,10 +27,10 @@ const TraineeApplicantSchema = new Schema({
       type: String,
       required: true,
     },
-    cycle_id:{
+    cycle_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "applicationCycle",
-      required: true
+      required: true,
     },
     delete_at: {
       type: Boolean,
@@ -38,21 +39,35 @@ const TraineeApplicantSchema = new Schema({
     cycleApplied: [CycleAppliedSchema],
     applicationPhase: {
       type: String,
-      enum: ["Applied", 'Shortlisted', 'Technical Assessment', 'Interview Assessment', 'Admitted', 'Rejected', "Enrolled"],
+      enum: [
+        "Applied",
+        "Shortlisted",
+        "Technical Assessment",
+        "Interview Assessment",
+        "Admitted",
+        "Rejected",
+        "Enrolled",
+      ],
       default: "Applied",
     },
     status: {
       type: String,
       default: "No action",
     },
+    role: {
+      type: Schema.Types.ObjectId,
+      ref: "Role",
+    },
     cohort: {
       type: Schema.Types.ObjectId,
       ref: "cohortModel",
-    }
-  },{
+    },
+  },
+  {
     timestamps: true,
-  });
+  }
+);
 
-const TraineeApplicant = mongoose.model('Trainees', TraineeApplicantSchema);
+const TraineeApplicant = mongoose.model("Trainees", TraineeApplicantSchema);
 
-  export default TraineeApplicant;
+export default TraineeApplicant;
