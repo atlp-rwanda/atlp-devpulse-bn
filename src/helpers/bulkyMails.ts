5b27cf5585f0056c00ec2a2e47f07db29a690735
NDAYISABYE Salim <81898585+Salim-54@users.noreply.github.com>
2024-12-09 09:49:44 +0200

    Revert "Ft schedule technical interview (#169)"
    This reverts commit eb83c2babc0c2dd4a7f18bcb15161b612b013854.

--- src/helpers/bulkyMails.ts
+++ src/helpers/bulkyMails.ts
@@ -111,12 +111,13 @@ export const sendUserCredentials = async (email: String, password: String) => {
   }
 };
 
+
 export const sendEmailTemplate = async (
   email: string,
   subject: string,
   title: string,
   body: string,
-  button?: { url: string; text: string }
+  button?: { url: string, text: string }
 ) => {
   try {
     const logoText = "DevPulse";
@@ -170,7 +171,7 @@ export const sendEmailTemplate = async (
         <div style="margin-top: 80px;">
           <p style="color: #555; font-size: 14px;">
             If you received this email by mistake, simply ignore it. <br />
-            For any questions, contact us at <a href="mailto: devpulsedev@gmail.com" style="color: ${secondaryColor};">samuel.nishimwe@andela.com</a>.
+            For any questions, contact us at <a href="mailto: samuel.nishimwe@andela.com" style="color: ${secondaryColor};">samuel.nishimwe@andela.com</a>.
           </p>
           <a href="" style="margin: 0 10px; display: inline-flex; align-items: center; text-decoration: none;">
             <img 
@@ -251,7 +252,7 @@ export const sendEmailTemplate = async (
           ${generateLogo(logoText, mainColor)}
           ${generateTitle(title)}
           ${generateBody(body)}
-          ${button ? generateButton(button.url, button.text) : ""}
+          ${button ? generateButton(button.url, button.text) : ''}
           ${generateFooterLogo()}
           ${generateSocialIcons()}
           ${generateFooter()}
--- src/models/ShortlistedSchema.ts
+++ src/models/ShortlistedSchema.ts
@@ -2,33 +2,28 @@ import mongoose, { Schema, Document } from "mongoose";
 
 interface IShortlisted extends Document {
   applicantId: mongoose.Schema.Types.ObjectId;
-  status: "No action" | "Moved" | "Rejected" | "Admitted";
+  status: "No action" | "Invited" | "Moved" | "Rejected" | "Admitted";
   comments?: string;
 }
 
-const shortlistedSchema = new Schema<IShortlisted>(
-  {
-    applicantId: {
-      type: Schema.Types.ObjectId,
-      ref: "Trainees",
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ["No action", "Invited", "Moved", "Rejected", "Admitted"],
-      default: "No action",
-    },
-    comments: {
-      type: String,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+const shortlistedSchema = new Schema<IShortlisted>({
+  applicantId: {
+    type: Schema.Types.ObjectId,
+    ref: "Trainees",
+    required: true,
 
-const Shortlisted = mongoose.model<IShortlisted>(
-  "Shortlisted",
-  shortlistedSchema
-);
+  },
+  status: {
+    type: String,
+    enum: ["No action", "Invited", "Moved", "Rejected", "Admitted"],
+    default: "No action",
+  },
+  comments: {
+    type: String,
+  },
+},{
+  timestamps: true,
+});
+
+const Shortlisted = mongoose.model<IShortlisted>("Shortlisted", shortlistedSchema);
 export default Shortlisted;
